a03345031256544dbf4c1359c34a02cb70d040a8
Austin Philp <austinphilp@gmail.com>
2023-02-23 09:58:15 -0800

    explicitly specify requirements

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import find_packages, setup
 
-with open("requirements.txt") as f:
-    required = f.read().splitlines()
-
 setup(
     name="hexgen",
     description="Realistic world generation via a hexmap for RPGs",
     packages=find_packages(),
-    install_requires=required,
+    install_requires=[
+        "Pillow==9.4.0",
+        "NumPy==1.24.2",
+    ],
 )
